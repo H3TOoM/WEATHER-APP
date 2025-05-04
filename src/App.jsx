188014a25517c71a6a26a8dac319a4607baf232f
Hatim <altorbed7@gmail.com>
2025-05-04 11:16:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { assets } from "./assets/assets";
+
+const App = () => {
+  const [location, setLocation] = useState({});
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  // Get country and city
+  useEffect(() => {
+    axios
+      .get("https://ipwho.is/")
+      .then((response) => setLocation(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  // Get weather data
+  useEffect(() => {
+    if (!location.city) return;
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location.city}&appid=17b1a6efeae3fa20a41b81c4a5ef12e4&units=metric`
+      )
+      .then((response) => {
+        setWeather(response.data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, [location]);
+
+  // Get weather image based on weather condition
+  const getWeatherImage = (weatherMain) => {
+    switch (weatherMain) {
+      case "Clear":
+        return assets.clear;
+      case "Clouds":
+        return assets.clouds;
+      case "Wind":
+        return assets.wind;
+      case "Rain":
+        return assets.rain;
+      case "Snow":
+        return assets.snow;
+      case "Drizzle":
+        return assets.drizzle;
+      default:
+        return assets.clouds;
+    }
+  };
+
+  return (
+    <div className="weather-container m-auto mt-10 rounded-lg shadow-lg max-w-[95%] sm:max-w-[600px]">
+      <h1 className="logo mt-2 text-center text-xl sm:text-2xl font-bold">
+        <span>WEATHER</span> APP
+      </h1>
+
+      {loading ? (
+        <p className="text-center text-xl p-10 text-[#F6F4EB]">Loading...</p>
+      ) : (
+        <div className="weather-details flex flex-col justify-center items-center capitalize p-4">
+          <img
+            src={getWeatherImage(weather.weather?.[0]?.main)}
+            className="w-32 h-32 sm:w-40 sm:h-40"
+          />
+          <p className="text-[30px] sm:text-[35px]">{weather.main?.temp} °C</p>
+          <p className="text-[30px] sm:text-[35px]">{location.city}</p>
+
+          <div className="weather-box flex flex-col sm:flex-row justify-between items-center gap-5 p-4 w-full sm:w-[400px]">
+            <div className="flex gap-3 items-center">
+              <img src={assets.humidity} className="w-10 h-10" />
+              <p className="text-lg sm:text-xl">
+                <span>{weather.main?.humidity}%</span> <br />
+                humidity
+              </p>
+            </div>
+
+            <div className="flex gap-3 items-center">
+              <img src={assets.wind} className="w-10" />
+              <p className="text-lg sm:text-xl">
+                <span>{weather.wind?.speed} km/h</span> <br />
+                wind speed
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/assets.js
@@ -0,0 +1,19 @@
+import clear from "./clear.png";
+import clouds from "./clouds.png";
+import drizzle from "./drizzle.png";
+import humidity from "./humidity.png";
+import mist from "./mist.png";
+import rain from "./rain.png";
+import snow from "./snow.png";
+import wind from "./wind.png";
+
+export const assets = {
+  clear,
+  clouds,
+  drizzle,
+  humidity,
+  mist,
+  rain,
+  snow,
+  wind,
+};
